4c16d7d1d5caa1415a6143d408fc33c9c83b6f58
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-04-16 19:46:28 -0400

    Removed unnecessary assets/code on front-end

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/App.css';
-import './css/grails.css';
-import './css/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
