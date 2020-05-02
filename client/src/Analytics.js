79aee87956da7e0ddde94562a9735e0aae0d8672
dsivie <david.sivieri@gmail.com>
2020-05-02 04:29:42 -0400

    Test to see if charts can be displayed in the client code.

--- client/src/Analytics.js
+++ client/src/Analytics.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {CLIENT_VERSION, REACT_VERSION, SERVER_URL} from './config';
 
-export default class Analytics extends Component{
-  render() {
-    return (
-        <div>
-          You are on Analytics page.
-        </div>
-    );
-  }
+export default class Analytics extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    <br/><br/><br/>
+                    This is the analytics page.
+                    <br/><br/><br/>
+                    <img
+                        src="http://localhost:8080/image/getDoubleSeriesRadarChart?illness=COVID19&symptoms=Headache,Sore throat,Lack of Taste,Difficulty breathing,Sputum Production,Lack of Smell,Rash"></img>
+                </p>
+            </div>
+        );
+    }
 }
