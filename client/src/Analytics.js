84d24493b4c3d02bd1c224dcd9146f968964773d
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-05-03 12:04:07 -0400

    Hooked up analytics chart to analytics page, UI improvements

--- client/src/Analytics.js
+++ client/src/Analytics.js
@@ -4,12 +4,21 @@ import {CLIENT_VERSION, REACT_VERSION, SERVER_URL} from './config';
 export default class Analytics extends Component {
     render() {
         return (
-            <div>
-                <br/><br/><br/><br/>
-                <h1>All Patient Symptoms Bar Chart</h1>
-                <img className='graphic-container' alt='Patient symptoms radar chart'
-                     src={SERVER_URL + '/image/getAllSymptomsBarChart?illness=COVID19'}/>
-            </div>
+            <table className='analytics-container shadow-lg'>
+                <tr>
+                    <h1>Analytics</h1>
+                    <hr/>
+                </tr>
+                <tr>
+                    <h2>COVID-19 Positive Patients' Symptoms Radar Chart</h2>
+                    <img className='graphic-container' src={SERVER_URL + "/image/getAllSymptomsRadarChart?illness=COVID19"}/>
+                    <hr/>
+                </tr>
+                <tr>
+                    <h2>COVID-19 Positive Patients' Most Reported Symptoms</h2>
+                    <img className='graphic-container' src={SERVER_URL + "/image/getAllSymptomsBarChart?illness=COVID19"}></img>
+                </tr>
+            </table>
         );
     }
 }
